5d7797e48124e33cbd321e2222875758ac0b9979
ynguyen2024tt <ynguyen2024tt@gmail.com>
2024-11-05 15:31:47 +0700

    feat: add database connect

---
+++ src/config/postgresql.js
@@ -0,0 +1,16 @@
+import pg from 'pg'
+import { env } from './enviroments'
+const { Pool } = pg()
+
+export const pool = new Pool
+    ({
+        user: env.DATABASE_USER,
+        host: env.LOCAL_DEV_APP_HOST,
+        database: env.DATABASE_NAME,
+        password: env.DATABASE_PASSWORD,
+        port: env.DATABASE_PORT
+    })
+
+
+
+
