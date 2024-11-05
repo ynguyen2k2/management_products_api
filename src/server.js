ca567288dc8cc2f4d58363e4f3e3de7a992f4f83
ynguyen2024tt <ynguyen2024tt@gmail.com>
2024-11-05 15:32:11 +0700

    wip

--- src/server.js
+++ src/server.js
@@ -1,13 +1,34 @@
 import express from 'express'
-
-const hostname = 'localhost'
-const port = 8017
-app.get('/', (req, res) => {
-    res.end('<h1>Hello World!</h1><hr>')
-})
+import { env } from './config/enviroments.js'
+import { pool } from './config/postgresql.js'
 const START_SERVER = () => {
     const app = express()
+    app.use(express.json())
+    if (env.BUILD_MODE === 'production') {
+        app.listen(process.env.PORT, () => {
+            // eslint-disable-next-line no-console
+            console.log(
+                `3. Production: Hi ${env.AUTHOR}, Back-end Server is running successfully at Port: ${process.env.PORT}/`
+            )
+        })
+    } else {
+        app.listen(env.LOCAL_DEV_APP_PORT, env.LOCAL_DEV_APP_HOST, () => {
+
+
+            // eslint-disable-next-line no-console
+            console.log(
+                `3. Local DEV Hi ${env.AUTHOR}, Back-end Server is running successfully at Host http://${env.LOCAL_DEV_APP_HOST}:${env.LOCAL_DEV_APP_PORT}/`
+            )
+        })
+    }
 }
-app.listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}/`)
-})
+
+
+(async () => {
+    try {
+        START_SERVER();
+        // const client = new pool.connect()
+    } catch (error) {
+        console.error(error)
+    }
+})()
