e67666c71b66d780c3c9f1c08d99ea70cef22998
ynguyen2024tt <ynguyen2024tt@gmail.com>
2024-11-06 13:53:33 +0700

    add v1 route index

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,1 +1,12 @@
-// index
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+const Router = express.Router()
+
+Router.get('/status', (req,res) => {
+    res.status(StatusCodes.Ok).json({
+        message: 'API V1 are ready to use.',
+        code:StatusCodes.Ok
+    })
+})
+
+export const APIs_V1 = Router
