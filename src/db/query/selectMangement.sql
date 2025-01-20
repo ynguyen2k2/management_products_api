8c55a691127db5764c9745d952bc9a50d7534a32
ynguyen2024tt <ynguyen2024tt@gmail.com>
2025-01-20 16:19:18 +0700

    add skus details query

--- src/db/query/selectMangement.sql
+++ src/db/query/selectMangement.sql
@@ -25,6 +25,22 @@ SELECT m.name as machinename, m.id as machineId, d.name as departmentName,d.id a
 select * from colorproduct;
 select * from products ;
 select * from productssku;
+
+--  query product
 select p.id as id ,p.name as name, ps.id as skuid ,ps.colorid as colorId, cl.value as color, ps.painttypeid as painttypeId, pt.value as painttype , ps.internalcode , p.description as description, p.cover as cover, p.slug as slug,p.createdat as createdat, p.updatedat as updatedat from products as p 
 inner join (productssku as ps INNER JOIN colorproduct AS cl on cl.id = ps.colorid
 							   INNER JOIN painttype as pt on pt.id = ps.painttypeid)on ps.productid = p.id where p.id = 1;
+
+-- query skus 
+select p.id as id ,p.name as name, ps.id as skuid ,ps.colorid as colorProductId, cl.value as colorProduct, ps.painttypeid as painttypeId, cm.id as componentId , 
+cm.name as componentName, clc.id as colorComponentId, clc.value as colorComponent, rm.id as rawMaterialid, rm.value as rawMeterial, pt.value as painttype , ps.internalcode,ps.createdat as createdat, ps.updatedat as updatedat from productssku as ps 
+INNER JOIN colorproduct AS cl on cl.id = ps.colorid
+INNER JOIN painttype as pt on pt.id = ps.painttypeid
+INNER JOIN products as p on p.id = ps.productid
+INNER JOIN (components as cm 
+	  INNER JOIN colorproduct as clc on clc.id = cm.colorid
+	  INNER JOIN rawmaterial as rm on rm.id = cm.rawmaterialid) on cm.productid = ps.id
+where ps.id = 1;
+
+select * from components;
+select * from rawmaterial;
