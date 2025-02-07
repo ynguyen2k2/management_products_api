c7f2437b187618a8f0d74dda8ab181423b9a064b
ynguyen2024tt <ynguyen2024tt@gmail.com>
2025-02-07 09:11:21 +0700

    validate params step

--- src/validations/product/validateStepComponentParams.js
+++ src/validations/product/validateStepComponentParams.js
@@ -2,6 +2,8 @@ import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
 
 const validateQuery = async (req, res, next) => {
+  const sku = parseInt(req.query.sku) || 0
+  const component = parseInt(req.query.component) || 0
   const limit = parseInt(req.query.limit) || 5
   const page = parseInt(req.query.page) || 1
 
@@ -9,6 +11,14 @@ const validateQuery = async (req, res, next) => {
     '🚀 ~ file: validatePaginationParams.js:11 ~ validateQuery ~  req.query:',
     req.query
   )
+  if (sku < 1) {
+    next(new ApiError(StatusCodes.BAD_REQUEST, 'Invalid SKU'))
+  }
+
+  if (component < 1) {
+    next(new ApiError(StatusCodes.BAD_REQUEST, 'Invalid Component'))
+  }
+
   if (limit > 100) {
     next(
       new ApiError(
