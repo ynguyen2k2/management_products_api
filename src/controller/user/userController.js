72bcdc235e089e1c815fcdd289ea8b615d74b47d
ynguyen2024tt <ynguyen2024tt@gmail.com>
2025-02-11 07:35:51 +0700

    add user controller

---
+++ src/controller/user/userController.js
@@ -0,0 +1,72 @@
+import { StatusCodes } from 'http-status-codes'
+import { userService } from '~/services/user/userService'
+
+const createNew = async (req, res, next) => {
+  try {
+    const createUser = await userService.createNew(req.body)
+
+    console.log('🚀 ~ createUser:', createUser)
+
+    res.status(StatusCodes.CREATED).json(createUser)
+  } catch (error) {
+    next(error)
+  }
+}
+
+const getDetails = async (req, res, next) => {
+  try {
+    const userId = req.params.id
+    const user = await userService.getDetails(userId)
+    res.status(StatusCodes.OK).json(user)
+  } catch (error) {
+    next(error)
+  }
+}
+
+const getAll = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 5
+    const page = parseInt(req.query.page) || 1
+    const sort = req.query.sort || 'asc'
+    const filter = req.query.filter || 'id'
+    const offset = (page - 1) * limit
+
+    const users = await userService.getAll({
+      limit,
+      offset,
+      sort,
+      filter
+    })
+    res.status(StatusCodes.OK).json(users)
+  } catch (error) {
+    next(error)
+  }
+}
+
+const update = async (req, res, next) => {
+  try {
+    const userId = req.params.id
+    const user = await userService.update(userId, req.body)
+    res.status(StatusCodes.OK).json(user)
+  } catch (error) {
+    next(error)
+  }
+}
+
+const deleteItem = async (req, res, next) => {
+  try {
+    const userId = req.params.id
+    const result = await userService.deleteItem(userId)
+    res.status(StatusCodes.OK).json(result)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const userController = {
+  createNew,
+  getDetails,
+  getAll,
+  update,
+  deleteItem
+}
