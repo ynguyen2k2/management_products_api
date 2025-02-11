c953ec7eb03e374f3a184692d1d56f0d4aa15bd2
ynguyen2024tt <ynguyen2024tt@gmail.com>
2025-02-11 07:37:00 +0700

    add query details step component

--- src/models/product/step/stepComponentModel.js
+++ src/models/product/step/stepComponentModel.js
@@ -70,7 +70,17 @@ const findOneById = async (id) => {
 const getDetails = async (id) => {
   try {
     // console.log('🚀 ~ file: productModel.js:43 ~ id:', id)
-    const getDetailsQuery = 'SELECT * FROM  stepcomponent WHERE id = $1 '
+    const getDetailsQuery = `select sc.id, c.id as componentid, c.name as componentname, ps.id as skuid, p.name as productname,  d.id as departmentid , d.name as departmentname, 
+    m.id as machineid , m.name as machinename , o.id as operationid , o.name as operationname, sc.step as step, 
+    sc.cycletime as cycletime, sc.quantitycycletime as quantitycycletime, sc.workerquantity as workerquantity, 
+    sc.createdat, sc.updatedat from stepcomponent as sc
+    INNER JOIN ( components as c
+	  INNER JOIN (productssku as ps 
+	  INNER JOIN products as p on p.id = ps.productid) on c.productid = ps.id ) on c.id = sc.componentid
+    INNER JOIN (operations as o 
+	  INNER JOIN departments as d ON o.departmentid = d.id
+	  INNER JOIN machines as m ON o.machineid = m.id) ON sc.operationid = o.id
+    WHERE sc.id = $1 `
     const client = await pool.connect()
 
     const result = await client.query(getDetailsQuery, [id])
