f430686276cf69499df639e7efc790818bea047f
ynguyen2024tt <ynguyen2024tt@gmail.com>
2025-02-12 07:51:03 +0700

    add auth user

--- src/routes/v1/user/userRoute.js
+++ src/routes/v1/user/userRoute.js
@@ -1,17 +1,33 @@
 import express from 'express'
+import { authController } from '~/controller/user/authController'
 import { userController } from '~/controller/user/userController'
 import { validatePaginationParams } from '~/validations/product/validatePaginationParams'
 import { userValidation } from '~/validations/user/userValidation'
 
 const Router = express.Router()
+Router.post('/login', authController.login)
+Router.post('/signup', authController.signup)
+Router.get('/logout', authController.logout)
 
+Router.post('/forgotPassword', authController.forgotPassword)
+Router.patch('/resetPassword/:token', authController.resetPassword)
+
+Router.use(authController.protect)
+
+Router.patch('/updateMypassword', authController.updatePassword)
+Router.route('/me').get(userController.getDetails)
+Router.route('/updateMe').patch(userValidation.update, userController.update)
+
+// Router.delete('/deleteMe', userController.deleteItem)
+
+//admin only
+Router.use(authController.restrictTo('admin'))
 Router.route('/')
   .get(validatePaginationParams.validateQuery, userController.getAll)
   .post(userValidation.createNew, userController.createNew)
-Router.route('/me').get(userController.getDetails)
+
 Router.route('/:id')
   .get(userController.getDetails)
-  // update product
   .patch(userValidation.update, userController.update)
   .delete(userController.deleteItem)
 
